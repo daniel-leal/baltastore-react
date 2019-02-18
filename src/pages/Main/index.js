0b0ea9ed9d10ed6b1ea92bb5edfb7d0972e90ea3
Daniel Leal <danielleal94@gmail.com>
2019-02-18 19:21:41 -0300

    add snack component

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,227 +1,260 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 // Styles
-import './styles.css'
+import "./styles.css";
 
 // Components
-import Header from '../../components/Header'
+import Header from "../../components/Header";
+import MySnackbarContentWrapper from "../../components/Snackbar";
 
 // Material
-import Table from '@material-ui/core/Table'
-import TableBody from '@material-ui/core/TableBody'
-import TableCell from '@material-ui/core/TableCell'
-import TableHead from '@material-ui/core/TableHead'
-import TableRow from '@material-ui/core/TableRow'
-import Paper from '@material-ui/core/Paper'
-import Button from '@material-ui/core/Button'
-import TextField from '@material-ui/core/TextField'
-import Dialog from '@material-ui/core/Dialog'
-import DialogActions from '@material-ui/core/DialogActions'
-import DialogContent from '@material-ui/core/DialogContent'
-import DialogTitle from '@material-ui/core/DialogTitle'
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import Snackbar from "@material-ui/core/Snackbar";
 
 // API
-import api from '../../services/api'
+import api from "../../services/api";
 
 export default class Main extends Component {
   state = {
     customers: [],
     errors: {},
+    successMsg: '',
     customer: {
-      FirstName: '',
-      LastName: '',
-      Document: '',
-      Email: '',
-      Phone: ''
+      FirstName: "",
+      LastName: "",
+      Document: "",
+      Email: "",
+      Phone: ""
     },
-    open: false
-  }
+    open: false,
+    openSnack: false
+  };
 
   async componentDidMount() {
     try {
-      const response = await api.get('/v1/customers')
-      this.setState({ customers: response.data })
+      const response = await api.get("/v1/customers");
+      this.setState({ customers: response.data });
     } catch (err) {}
   }
 
   // DIALOG FUNCTIONS
   handleClickOpen = () => {
-    this.setState({ open: true })
-  }
+    this.setState({ open: true });
+  };
   handleClose = () => {
-    this.clearForm()
-    this.clearError()
+    this.clearForm();
+    this.clearError();
     this.setState({
       open: false
-    })
-  }
+    });
+  };
+
+  // SNACKBAR FUNCTIONS
+  handleCloseSnack = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    this.setState({ openSnack: false });
+  };
+  handleClick = () => {
+    this.setState({ openSnack: true });
+  };
 
   // TWO-WAY DATABIND
   handleFirstNameChange = e => {
-    const { customer } = this.state
+    const { customer } = this.state;
     this.setState({
       customer: {
         ...customer,
         FirstName: e.target.value
       }
-    })
-  }
+    });
+  };
   handleLastNameChange = e => {
-    const { customer } = this.state
+    const { customer } = this.state;
     this.setState({
       customer: {
         ...customer,
         LastName: e.target.value
       }
-    })
-  }
+    });
+  };
   handleDocumentChange = e => {
-    const { customer } = this.state
+    const { customer } = this.state;
     this.setState({
       customer: {
         ...customer,
         Document: e.target.value
       }
-    })
-  }
+    });
+  };
   handleEmailChange = e => {
-    const { customer } = this.state
+    const { customer } = this.state;
     this.setState({
       customer: {
         ...customer,
         Email: e.target.value
       }
-    })
-  }
+    });
+  };
   handlePhoneChange = e => {
-    const { customer } = this.state
+    const { customer } = this.state;
     this.setState({
       customer: {
         ...customer,
         Phone: e.target.value
       }
-    })
-  }
+    });
+  };
 
   // FORM ACTIONS
   clearForm = () => {
     this.setState({
       customer: {
-        FirstName: '',
-        LastName: '',
-        Document: '',
-        Email: '',
-        Phone: ''
+        FirstName: "",
+        LastName: "",
+        Document: "",
+        Email: "",
+        Phone: ""
       }
-    })
-  }
+    });
+  };
   clearError = () => {
     this.setState({
       errors: {
         firstNameError: false,
-        firstNameErrorMsg: '',
+        firstNameErrorMsg: "",
         lastNameError: false,
-        lastNameErrorMsg: '',
+        lastNameErrorMsg: "",
         documentError: false,
-        documentErrorMsg: '',
+        documentErrorMsg: "",
         emailError: false,
-        emailErrorMsg: '',
+        emailErrorMsg: "",
         phoneError: false,
-        phoneErrorMsg: ''
+        phoneErrorMsg: ""
       }
-    })
-  }
+    });
+  };
   save = async () => {
     try {
-      var response = await api.post('/v1/customers', this.state.customer)
+      var response = await api.post("/v1/customers", this.state.customer);
 
       this.setState({
-        customers: [...this.state.customers, response.data.data]
-      })
+        customers: [...this.state.customers, response.data.data],
+        successMsg: response.data.message,
+      });
 
-      this.handleClose()
+      this.handleClose();
+      this.handleClick();
     } catch (error) {
-      this.clearError()
-      this.validate(error.response.data.data)
+      this.clearError();
+      this.validate(error.response.data.data);
     }
-  }
+  };
 
   validate = async errors => {
     let properties = {
       FirstNameError: false,
-      FirstNameErrorMsg: '',
+      FirstNameErrorMsg: "",
       LastNameError: false,
-      LastNameErrorMsg: '',
+      LastNameErrorMsg: "",
       DocumentError: false,
-      DocumentErrorMsg: '',
+      DocumentErrorMsg: "",
       EmailError: false,
-      EmailErrorMsg: '',
+      EmailErrorMsg: "",
       PhoneError: false,
-      PhoneErrorMsg: ''
-    }
+      PhoneErrorMsg: ""
+    };
 
-    if (errors.find(x => x.property === 'FirstName')) {
-      properties.FirstNameError = true
+    if (errors.find(x => x.property === "FirstName")) {
+      properties.FirstNameError = true;
       properties.FirstNameErrorMsg = errors.find(
-        x => x.property === 'FirstName'
-      ).message
+        x => x.property === "FirstName"
+      ).message;
     }
 
-    if (errors.find(x => x.property === 'LastName')) {
-      properties.LastNameError = true
+    if (errors.find(x => x.property === "LastName")) {
+      properties.LastNameError = true;
       properties.LastNameErrorMsg = errors.find(
-        x => x.property === 'LastName'
-      ).message
+        x => x.property === "LastName"
+      ).message;
     }
 
-    if (errors.find(x => x.property === 'Email')) {
-      properties.EmailError = true
+    if (errors.find(x => x.property === "Email")) {
+      properties.EmailError = true;
       properties.EmailErrorMsg = errors.find(
-        x => x.property === 'Email'
-      ).message
+        x => x.property === "Email"
+      ).message;
     }
 
-    if (errors.find(x => x.property === 'Document')) {
-      properties.DocumentError = true
+    if (errors.find(x => x.property === "Document")) {
+      properties.DocumentError = true;
       properties.DocumentErrorMsg = errors.find(
-        x => x.property === 'Document'
-      ).message
+        x => x.property === "Document"
+      ).message;
     }
 
-    if (errors.find(x => x.property === 'Phone')) {
-      properties.PhoneError = true
+    if (errors.find(x => x.property === "Phone")) {
+      properties.PhoneError = true;
       properties.PhoneErrorMsg = errors.find(
-        x => x.property === 'Phone'
-      ).message
+        x => x.property === "Phone"
+      ).message;
     }
 
-    this.setState({ errors: properties })
-  }
-
+    this.setState({ errors: properties });
+  };
 
   render() {
     return (
-      <div className='container'>
-        <Header title='Customers' />
+      <div className="container">
+        <Header title="Customers" />
 
-        <div className='ajusteTopo'>
+        <Snackbar
+          anchorOrigin={{
+            vertical: "top",
+            horizontal: "right"
+          }}
+          open={this.state.openSnack}
+          autoHideDuration={6000}
+          onClose={this.handleCloseSnack}
+        >
+          <MySnackbarContentWrapper
+            onClose={this.handleCloseSnack}
+            variant="success"
+            message={this.state.successMsg}
+          />
+        </Snackbar>
+
+        <div className="ajusteTopo">
           <Paper>
             <Table>
               <TableHead>
                 <TableRow>
                   <TableCell>Nome</TableCell>
-                  <TableCell align='center'>CPF</TableCell>
-                  <TableCell align='center'>Email</TableCell>
+                  <TableCell align="center">CPF</TableCell>
+                  <TableCell align="center">Email</TableCell>
                 </TableRow>
               </TableHead>
               <TableBody>
                 {this.state.customers.map(customer => (
                   <TableRow key={customer.id}>
-                    <TableCell component='th' scope='row'>
+                    <TableCell component="th" scope="row">
                       {customer.name}
                     </TableCell>
-                    <TableCell align='center'>{customer.document}</TableCell>
-                    <TableCell align='center'>{customer.email}</TableCell>
+                    <TableCell align="center">{customer.document}</TableCell>
+                    <TableCell align="center">{customer.email}</TableCell>
                   </TableRow>
                 ))}
               </TableBody>
@@ -233,15 +266,15 @@ export default class Main extends Component {
           <Dialog
             open={this.state.open}
             onClose={this.handleClose}
-            aria-labelledby='form-dialog-title'
+            aria-labelledby="form-dialog-title"
           >
-            <DialogTitle id='form-dialog-title'>Inclusão</DialogTitle>
+            <DialogTitle id="form-dialog-title">Inclusão</DialogTitle>
             <DialogContent>
               <TextField
                 autoFocus
-                margin='dense'
-                label='First Name'
-                type='text'
+                margin="dense"
+                label="First Name"
+                type="text"
                 value={this.state.customer.FirstName}
                 onChange={this.handleFirstNameChange}
                 error={this.state.errors.FirstNameError}
@@ -250,9 +283,9 @@ export default class Main extends Component {
               />
 
               <TextField
-                margin='dense'
-                label='Last Name'
-                type='text'
+                margin="dense"
+                label="Last Name"
+                type="text"
                 value={this.state.customer.LastName}
                 onChange={this.handleLastNameChange}
                 error={this.state.errors.LastNameError}
@@ -260,10 +293,10 @@ export default class Main extends Component {
                 fullWidth
               />
               <TextField
-                margin='dense'
-                name='document'
-                label='CPF'
-                type='text'
+                margin="dense"
+                name="document"
+                label="CPF"
+                type="text"
                 value={this.state.customer.Document}
                 onChange={this.handleDocumentChange}
                 error={this.state.errors.DocumentError}
@@ -271,10 +304,10 @@ export default class Main extends Component {
                 fullWidth
               />
               <TextField
-                margin='dense'
-                id='name'
-                label='Email Address'
-                type='email'
+                margin="dense"
+                id="name"
+                label="Email Address"
+                type="email"
                 value={this.state.customer.Email}
                 onChange={this.handleEmailChange}
                 error={this.state.errors.EmailError}
@@ -282,10 +315,10 @@ export default class Main extends Component {
                 fullWidth
               />
               <TextField
-                margin='dense'
-                id='name'
-                label='Phone'
-                type='tel'
+                margin="dense"
+                id="name"
+                label="Phone"
+                type="tel"
                 value={this.state.customer.Phone}
                 onChange={this.handlePhoneChange}
                 error={this.state.errors.PhoneError}
@@ -294,26 +327,26 @@ export default class Main extends Component {
               />
             </DialogContent>
             <DialogActions>
-              <Button onClick={this.handleClose} color='primary'>
+              <Button onClick={this.handleClose} color="primary">
                 Cancel
               </Button>
-              <Button onClick={this.save} color='primary' variant='contained'>
+              <Button onClick={this.save} color="primary" variant="contained">
                 Incluir
               </Button>
             </DialogActions>
           </Dialog>
         </div>
 
-        <div className='buttonTopo'>
+        <div className="buttonTopo">
           <Button
-            variant='contained'
-            color='primary'
+            variant="contained"
+            color="primary"
             onClick={this.handleClickOpen}
           >
             Incluir
           </Button>
         </div>
       </div>
-    )
+    );
   }
 }
